186c5c95b9714f5417c46cb4347a584ceb6c0b4b
Vinicius da Silva Viana <vinisilva.viana87@gmail.com>
2023-07-29 14:19:41 -0300

    Tabuada

---
+++ scriptdomodelo.js
@@ -0,0 +1,20 @@
+function tabu() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+         window.alert('Por favor digite um número!')
+    }else{
+        let n = Number(num.value)
+        let c = 1   
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let  item = document.createElement('option') 
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+     }
+
+   
+}
